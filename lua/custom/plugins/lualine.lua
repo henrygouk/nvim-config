5f1eda8350f4eab157638dbb5ace6258342c559e
Henry Gouk <henry.gouk@gmail.com>
2024-08-10 20:05:19 +0100

    Update lualine config to better integrate with neo-tree

--- lua/custom/plugins/lualine.lua
+++ lua/custom/plugins/lualine.lua
@@ -3,12 +3,16 @@ return {
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   config = function()
     require('lualine').setup {
+      options = {
+        globalstatus = true,
+      },
       sections = {
         lualine_x = {
           { 'copilot', show_colors = true },
           'filetype',
         },
       },
+      extensions = { 'neo-tree', 'lazy' },
     }
   end,
 }
